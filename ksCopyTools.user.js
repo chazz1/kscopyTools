f772ba72b5178d9727a132c1ee72dc076c667b23
chazz <648637246@qq.com>
2021-12-31 20:21:10 +0800

    Add files via upload

--- ksCopyTools.user.js
+++ ksCopyTools.user.js
@@ -2,43 +2,54 @@
 // @name         ksCopyTools
 // @name:zh      快手工具 chazz
 // @namespace    https://gitee.com/chazzcfb/kscopyTools
-// @version      0.1
-// @description  快手辅助工具测试
+// @version      0.2
+// @description  快手辅助工具
 // @author       chazz <chazzcfb@163.com>
-// @match        *://s.kwaixiaodian.com/*
+// @match        *://s.kwaixiaodian.com/zone-origin/order/detail*
 // @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.2.1/jquery.min.js
 // @require      https://cdn.bootcdn.net/ajax/libs/clipboard.js/2.0.6/clipboard.min.js
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-    $(document).ready(function(){
+    $(document).ready(function () {
 
         $("body").append('<div style="position: fixed;min-width: 100px;z-index: 9999999999;padding: 10px;bottom: 500px;right: 25px;" id="ch-box"><button style="color:#fff;width:100px;line-height: 40px;background-color:red;font-size:18px;border: 1px;" id="ch-bang" value=0>提取数据</button></div>');
 
 
-        $("#ch-bang").click(function() {
+        $("#ch-bang").click(function () {
             var btnval = $("#ch-bang").val();//获取按钮状态0位提取数据，1为复制数据
-            if(btnval==0){
+            if (btnval == 0) {
                 //提取数据，然后点复制按钮
                 var obj = $("span.ant-descriptions-item-content");
                 var expressNo = $(".express-no").text();
-                var expressCompany =  $(".express-company").text();
+                var expressCompany = $(".express-company").text();
                 var orderNo = obj.eq(0).text();// 订单编号
-                var userInfo =obj.eq(6).text().split(','); //用户收货信息
-
-                var txts =orderNo + "\t" +userInfo[0] + "\t"+userInfo[2] + "\t"+  expressCompany+ "\t"+expressNo;
+                var userInfo = obj.eq(6).text().split(','); //用户收货信息
+                var addir = trim(userInfo[0]);
+                var txts = orderNo + "\t" + addir + "\t" + userInfo[2] + "\t" + expressCompany + "\t" + expressNo;
                 console.log(txts);
-                $("#ch-bang").attr("data-clipboard-text", txts).css("background-color","Coral").text("复制数据").val(1);
-            }else{
-                $("#ch-bang").text("提取数据").css("background-color","red").val(0);
+                $("#ch-bang").attr("data-clipboard-text", txts).css("background-color", "DodgerBlue").text("复制数据").val(1);
+            } else {
+                $("#ch-bang").text("提取数据").css("background-color", "red").val(0);
             }
 
             copy();
-        } )
+        })
 
     });
+    //去除字符两边的空白字符
+
+    function trim(s) {
+
+        //    \s表示匹配任何空白字符，包括空格、制表符、换页符等
+        //    ^\s*匹配字符串前的所有空白字符
+        //    \s*$匹配字符串后的所有空白字符
+        //     g表示尽可能多的匹配结果
+        var str = s.replace(/(^\s*)|(\s*$)/g, "");
+        return str;
+    }
     /**
  * 拷贝方法
  */
